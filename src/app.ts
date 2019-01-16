bf13269dff4ed66eb710614b74bb19f3a01e1c32
Andy Wermke <andy.dev@next-step-software.com>
2019-01-16 10:46:12 +0100

    Respond with proper JSON errors for 4xx errors

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,27 @@ import url from "url"
 import { Config } from "./config"
 import createRouter from "./router"
 
+const ErrorMiddleware = () => async (ctx: Koa.Context, next: () => Promise<any>) => {
+  try {
+    return await next()
+  } catch (error) {
+    if (error.status && error.status >= 400 && error.status < 500) {
+      const body: any = {
+        message: error && error.message ? error.message : "Unknown error"
+      }
+      for (const prop of ["data", "response"]) {
+        if (prop in error) {
+          body[prop] = error[prop]
+        }
+      }
+      ctx.response.body = body
+    } else {
+      // re-throw
+      throw error
+    }
+  }
+}
+
 export default function createApp(config: Config) {
   const app = new Koa()
   const router = createRouter(config)
@@ -14,6 +35,7 @@ export default function createApp(config: Config) {
 
   return app
     .use(CORS())
+    .use(ErrorMiddleware())
     .use(router.routes())
     .use(router.allowedMethods())
 }
